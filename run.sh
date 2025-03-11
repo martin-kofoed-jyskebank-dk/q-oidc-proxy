23d0b9903a6127768426941ee49e3f6c89318a16
Martin Kofoed <jb2948@jyskebank.dk>
2025-03-11 14:39:24 +0100

    Removed bin folder

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ export AUTHENTICATION_COOKIE_SAMESITE=LAX
 export AUTHENTICATION_FRONTEND_REDIRECT=http://localhost:5173
 export AUTHENTICATION_FRONTEND_CALLBACK_PARAM=authcode
 export AUTHENTICATION_BACKEND_HEADER_NAME=Authorization
-export AUTHENTICATION_ALLOW_ORIGINS="/https://([a-z0-9\\-_]+)\\.my\\.company\\.net/"
+export AUTHENTICATION_ALLOW_ORIGINS="/https://([a-z0-9\\-_]+)localhost/"
 export OIDC_PROVIDER_CONF_URL=http://localhost:9090/.well-known/openid-configuration
 export OIDC_AUTH_URI_TEMPLATE=/auth?response_type=%s&client_id=%s&scope=openid&redirect_uri=%s&state=%s
 export OIDC_RESPONSE_TYPE=code
@@ -17,7 +17,7 @@ export OIDC_CLIENT_SECRET=my-client-secret
 export OIDC_REDIRECT_URI=http://localhost:8080/oidc/callback
 
 export BACKEND_HOST_BASE_URL=http://localhost:8081
-export BACKEND_HEADER_PROPAGATION=Authorization,corrid,x-app-version,x-app-platform,x-app-device
+export BACKEND_HEADER_PROPAGATION=Authorization, X-Correlation-Id
 
 # Run quarkus in dev mode
 quarkus dev 
