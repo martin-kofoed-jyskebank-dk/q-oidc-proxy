e22c61ad379ecdd8c5b7a88295b028ea182984e5
Martin Kofoed <jb2948@jyskebank.dk>
2025-08-14 08:33:55 +0200

    Changed mapping of client call exceptions a little. Bumped dependencies,

--- src/main/java/dk/kofoed/proxy/exception/mapping/ExceptionResponseMapper.java
+++ src/main/java/dk/kofoed/proxy/exception/mapping/ExceptionResponseMapper.java
@@ -18,20 +18,24 @@ public class ExceptionResponseMapper {
      * Map <code>ProxyClientException</code> to an HTTP response.
      */
     @ServerExceptionMapper
-    public Response mapException(ProcessingException e) throws InterruptedException {
-        if (e.getCause() instanceof ProxyClientException ex) {
-
-            // If we receive a 401 from backend calls, it means that we passed a useless access_token. This happens
-            // when we're still trying to refresh the token but OIDC token endpoint reply isn't in yet (async call).
-            // When this occurs, we instruct the client to try again after a small delay:
-            if (ex.getStatusCode() == 401) {
-                Thread.sleep(200);
-                return Response.temporaryRedirect(uriInfo.getRequestUri()).build();
-            } else {
-                return Response.status(ex.getStatusCode()).entity(ex.getResponseBody()).build();
-            }
+    public Response mapException(ProxyClientException e) throws InterruptedException {
+        // If we receive a 401 from backend calls, it means that we passed a useless access_token. This happens
+        // when we're still trying to refresh the token but OIDC token endpoint reply isn't in yet (async call).
+        // When this occurs, we instruct the client to try again after a small delay:
+        if (e.getStatusCode() == 401) {
+            Thread.sleep(200);
+            return Response.temporaryRedirect(uriInfo.getRequestUri()).build();
+        } else {
+            return Response.status(e.getStatusCode()).entity(e.getResponseBody()).build();
         }
-        return Response.serverError().build();
+    }
+
+    /**
+     * Map <code>ProcessingException</code> to HTTP response.
+     */
+    @ServerExceptionMapper
+    public Response mapException(ProcessingException e) {
+        return Response.status(500).build();
     }
 
 }
